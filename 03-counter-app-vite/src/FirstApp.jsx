1cf9d6017dd193b2c978bbe366623690c8e5dbc2
NilssonLoRi <nlopez153@misena.edu.co>
2023-12-03 13:55:48 -0500

    Video 44 Tarea - Componente CounterApp

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,44 +1,56 @@
+import PropTypes  from 'prop-types'
 // const newMessage = {
-//     message: 'Hola Mundo',
-//     title: 'Nilson'
-// }
+  //     message: 'Hola Mundo',
+  //     title: 'Nilson'
+  // }
+  
+  const getResult = () => {
+    return ({
+      // <div>FirstApp</div>
+      message: 'Hola Mundo',
+      title: 'El gato',
+      text: 'internal test text',
+    })
+  }
+  
 
-const getResult = () => {
-  return ({
-    // <div>FirstApp</div>
-    message: 'Hola Mundo',
-    title: 'El gato',
-    text: 'internal test text',
-  })
-}
+const FirstApp = ( { title, subTitle, name } ) => {
+  
+  //console.log ( props )
+  
+    /* if ( !title ) {
+      throw new Error('El title no existe.');
+    } */
+    // Para evitar colocar todo este codigo (el if) se usan las PropTypes en este caso para validar que no exista un titulo.
 
-
-const FirstApp = ( { title, subTitle } ) => {
-    
-    //console.log ( props )
-
-    // No es recomentable que este valor este dentro de este functional component
-    // es mejor sacarlo aunque eso no significa que vaya a ser un valor dentro del scope global,
-    // por que el queda encapsulado dentro de FirstApp.jsx
-
-    return (
-
-        
+    return (        
         <> {/* Esta es la abreviación de Fragment: <></> */}
 
-            <h1> { title } </h1>
+        <h1> { title } </h1>
 
-            {/* <code>First App by { JSON.stringify( newMessage ) } </code> */}
+        <code> { JSON.stringify( getResult() ) } </code>
 
-            <code> { JSON.stringify( getResult() ) } </code> 
+        {/* el JSON.stringify() me ayuda a imprimir una funcion en este caso con multiples 
+        "cosas"  dentro y el <code></code> es simplemtente para que se vea mas bonito,
+        facilmente puede ir un <h1></h1> o un <p></p>*/}
 
-            {/* el JSON.stringify() me ayuda a imprimir una funcion en este caso con multiples 
-            "cosas"  dentro y el <code></code> es simplemtente para que se vea mas bonito,
-            facilmente puede ir un <h1></h1> o un <p></p>*/}
-
-            <p> { subTitle + 7 } </p>
+        <p> { subTitle } </p>
+        <p> { name } </p>
+        
         </>
+
     )
+}
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired, //este PropTypes me indica que title deber ser string y es requerido y/o/u obligatorio
+  subTitle: PropTypes.string.isRequired,
+}
+
+FirstApp.defaultProps = {
+  title: 'No hay titulo',
+  subTitle: 'Ho hay subtitulo',
+  name: 'Nilson LoRito',
 }
 
 export default FirstApp
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import FirstApp from './FirstApp'
 import './styles.css'
-//import { HelloWorldApp } from './HelloWorldApp'
+// import { HelloWorldApp } from './HelloWorldApp'
+// import FirstApp from './FirstApp'
+import { CounterApp } from './CounterApp'
+
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <FirstApp title="Hola soy Goku" subTitle={123}/>
+        < CounterApp value={123}/>
     </React.StrictMode>
 )
