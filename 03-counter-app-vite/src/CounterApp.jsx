3773ef5dfefc6af30b3ee68d37657f7f39a7f8fa
NilssonLoRi <nlopez153@misena.edu.co>
2023-12-03 17:19:20 -0500

    Video 46 hooks

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,10 +1,30 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types'
+ 
 
 export const CounterApp = ({value}) => {
+  
+  // function handleAdd(event, newString) {
+  //   console.log(event)
+  //   console.log(newString)
+  // }
+
+  const [ counter, setCounter ] = useState( value );
+
+  const handleAdd = () => {
+    // console.log(event)
+    setCounter( counter + 1 )
+  }
+  
   return (
     <>
         <h1>CounterApp</h1>
-        <h2>{value}</h2>
+        <h2> { counter } </h2>
+
+        {/* <button onClick={ (event) => handleAdd(event, 'Hola mundos') } > (event) => handleAdd(event) */}
+        <button onClick={ handleAdd } >
+          + 1
+        </button>
     </>
   )
 }
